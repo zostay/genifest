f8d40313e6ecf905b507496830ec0603952a9b23
Sterling Hanenkamp <sterling@hanenkamp.com>
2024-06-27 00:40:55 -0500

    chore: fixing the install script

--- tools/install.sh
+++ tools/install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 INSTALL_DIR=/usr/local/bin
-CURRENT_VERSION=$(curl -s https://api.github.com/repos/zostay/genifest/releases/latest | grep tag_name | cut -d '"' -f 4)
+CURRENT_VERSION=$(curl -s https://api.github.com/repos/zostay/genifest/releases/latest | grep tag_name | cut -d '"' -f 4 | cut -c 2-)
 OS_NAME=$(uname -s | tr '[:upper:]' '[:lower:]')
 OS_ARCH=$(uname -m)
 DOWNLOAD_URL="https://github.com/zostay/genifest/releases/download/$CURRENT_VERSION/genifest-$CURRENT_VERSION-$OS_NAME-$OS_ARCH"
 
-curl -L "$DOWNLOAD_URL" -o "$INSTALL_DIR/genifest"
+sudo sh -c 'curl -L "$DOWNLOAD_URL" -o "$INSTALL_DIR/genifest" && chmod +x "$INSTALL_DIR/genifest"'
